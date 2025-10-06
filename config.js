e784e633a22c1de201cf6c9e22dece8d0ca1d338
mohamedtube608-ui <mohamedtube608@gmail.com>
2025-10-06 15:34:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     bot: {
         tokens: [
-          "MTQyNDc2NDAxOTQ0MDYxNTQ2NA.Gpx_VD.iWn-JLY86HeaTaf1NI0U5kZzhT-6cVgIw0NVDU",              // Main bot token - Replace with your actual token
+          "YOUR_BOT_TOKEN_HERE",              // Main bot token - Replace with your actual token
         //  "YOUR_SECOND_BOT_TOKEN_HERE",      // 2 bot token - Uncomment and replace to use
         //  "YOUR_THIRD_BOT_TOKEN_HERE",       // 3 bot token - Uncomment and replace to use
         //  "YOUR_FOURTH_BOT_TOKEN_HERE",      // 4 bot token - Uncomment and replace to use
@@ -36,4 +36,5 @@ module.exports = {
     }
 
 };
+
 
